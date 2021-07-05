93e8ed1db38d16809ab8d8b082d06509cbfeae20
akirat1412 <60830773+akirat1412@users.noreply.github.com>
2021-07-05 06:23:53 -1000

    more rules
    added check, castling, promotion, en passant

--- game.py
+++ game.py
@@ -1,13 +1,14 @@
 from piece import Piece
-import copy
 
 
 class Game:
     def __init__(self):
         self.board = [[Piece() for _ in range(8)] for _ in range(8)]
+        self.status = 'in_progress'
         self.turn = 'white'
         self.en_passant = None
         self.castling = [[True, True], [True, True]]
+        self.check_square = []
 
         self.initialize_board()
 
@@ -42,53 +43,68 @@ class Game:
         for i in range(8):
             self.board[i][6] = Piece('black', 'pawn')
 
-    # checks all squares that one side is attacking
-    def get_all_attacking(self, color):
+    # finds all squares that one side is attacking
+    def get_attacking(self, color):
         attacking = []
+
         if color == 'white':
-            for x, y in self.white_pieces:
-                attacking += self.get_attacking(x, y)
+            attacking_pieces = self.white_pieces
+        else:
+            attacking_pieces = self.black_pieces
 
-    # helper function that checks what squares a piece is attacking
-    def get_attacking(self, x, y, color):
-        piece = self.board[x][y]
-        if piece.type is None:
-            return []
-        elif piece.type == 'pawn':
-            if piece.color == 'white':
-                return [[x-1, y+1], [x+1, y+1]]
-            else:
-                return [[x-1, y-1], [x+1, y-1]]
-        elif piece.type == 'knight':
-            temp = [[x+1, y+2], [x+2, y+1], [x+2, y-1], [x+1, y-2], [x-1, y-2], [x-2, y-1], [x-2, y+1], [x-1, y+2]]
-            return filter(lambda square: -1 < square[0] < 8 and -1 < square[1] < 8, temp)
-        elif piece.type == 'bishop':
-            return []
-        elif piece.type == 'rook':
-            return []
-        elif piece.type == 'queen':
-            return []
-        elif piece.type == 'king':
-            return []
+        for x, y in attacking_pieces:
+            piece = self.board[x][y]
+            if piece.piece == 'pawn':
+                if color == 'white':
+                    attacking += [[x-1, y+1], [x+1, y+1]]
+                else:
+                    attacking += [[x-1, y-1], [x+1, y-1]]
+            elif piece.piece == 'knight':
+                attacking += [[x+1, y+2], [x+2, y+1], [x+2, y-1], [x+1, y-2],
+                              [x-1, y-2], [x-2, y-1], [x-2, y+1], [x-1, y+2]]
+            elif piece.piece == 'bishop':
+                directions = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
+                attacking += self.get_attacking_helper(x, y, directions)
+            elif piece.piece == 'rook':
+                directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+                attacking += self.get_attacking_helper(x, y, directions)
+            elif piece.piece == 'queen':
+                directions = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
+                attacking += self.get_attacking_helper(x, y, directions)
+            elif piece.piece == 'king':
+                attacking += [[x, y+1], [x+1, y+1], [x+1, y], [x+1, y-1],
+                              [x, y-1], [x-1, y-1], [x-1, y], [x-1, y+1]]
+        return attacking
 
-        return []
+    def get_attacking_helper(self, x, y, directions):
+        attacking = []
+        for dir_x, dir_y in directions:
+            temp_x = x + dir_x
+            temp_y = y + dir_y
 
-    def is_king_safe(self, start, end, color):
+            while -1 < temp_x < 8 and -1 < temp_y < 8:
+                attacking.append([temp_x, temp_y])
+                if self.board[temp_x][temp_y].color is not None:
+                    break
+                temp_x += dir_x
+                temp_y += dir_y
+        return attacking
+
+    # checks if a move puts the king in check
+    def is_king_safe(self, start, end):
         attacked = []
 
-        attacking_pieces = []
-        if color == 'white':
+        if self.turn == 'white':
             attacking_pieces = self.black_pieces
         else:
             attacking_pieces = self.white_pieces
 
         for x, y in attacking_pieces:
-            # TODO: en passant
             if end == [x, y]:
                 continue
             piece = self.board[x][y]
             if piece.piece == 'pawn':
-                if color == 'white':
+                if self.turn == 'white':
                     attacked += [[x-1, y-1], [x+1, y-1]]
                 else:
                     attacked += [[x-1, y+1], [x+1, y+1]]
@@ -97,17 +113,17 @@ class Game:
                              [x-1, y-2], [x-2, y-1], [x-2, y+1], [x-1, y+2]]
             elif piece.piece == 'bishop':
                 directions = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
-                attacked += self.is_king_safe_helper(x, y, start, end, directions, color)
+                attacked += self.is_king_safe_helper(x, y, start, end, directions)
             elif piece.piece == 'rook':
                 directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-                attacked += self.is_king_safe_helper(x, y, start, end, directions, color)
+                attacked += self.is_king_safe_helper(x, y, start, end, directions)
             elif piece.piece == 'queen':
                 directions = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
-                attacked += self.is_king_safe_helper(x, y, start, end, directions, color)
+                attacked += self.is_king_safe_helper(x, y, start, end, directions)
             elif piece.piece == 'king':
                 attacked += [[x, y+1], [x+1, y+1], [x+1, y], [x+1, y-1],
                              [x, y-1], [x-1, y-1], [x-1, y], [x-1, y+1]]
-        if color == 'white':
+        if self.turn == 'white':
             if start == self.white_king:
                 return end not in attacked
             else:
@@ -118,7 +134,7 @@ class Game:
             else:
                 return self.black_king not in attacked
 
-    def is_king_safe_helper(self, x, y, start, end, directions, color):
+    def is_king_safe_helper(self, x, y, start, end, directions):
         attacked = []
         for dir_x, dir_y in directions:
             temp_x = x + dir_x
@@ -133,8 +149,12 @@ class Game:
                 temp_y += dir_y
         return attacked
 
-    # checks if a move is legal
+    # checks if move is legal (valid AND does not put king in check)
     def is_legal(self, start, end):
+        return end in self.get_legal_moves(start) and self.is_king_safe(start, end)
+
+    # checks if a move is legal, but not whether it puts king in check
+    def get_legal_moves(self, start):
         x = start[0]
         y = start[1]
         piece = self.board[x][y]
@@ -150,6 +170,9 @@ class Game:
                     legal_moves.append([x - 1, y + 1])
                 if x < 7 and self.board[x + 1][y + 1].color == 'black':
                     legal_moves.append([x + 1, y + 1])
+                if self.en_passant is not None and y == 4 and (x-1 == self.en_passant or x+1 == self.en_passant) and \
+                        self.is_en_passant_safe(x):
+                    legal_moves.append([self.en_passant, 5])
             else:
                 if self.board[x][y - 1].piece is None:
                     legal_moves.append([x, y - 1])
@@ -159,6 +182,9 @@ class Game:
                     legal_moves.append([x - 1, y - 1])
                 if x < 7 and self.board[x + 1][y - 1].color == 'white':
                     legal_moves.append([x + 1, y - 1])
+                if self.en_passant is not None and y == 3 and (x-1 == self.en_passant or x+1 == self.en_passant) and \
+                        self.is_en_passant_safe(x):
+                    legal_moves.append([self.en_passant, 2])
         elif piece.piece == 'knight':
             temp = [[x+1, y+2], [x+2, y+1], [x+2, y-1], [x+1, y-2],
                     [x-1, y-2], [x-2, y-1], [x-2, y+1], [x-1, y+2]]
@@ -178,8 +204,26 @@ class Game:
                     [x, y-1], [x-1, y-1], [x-1, y], [x-1, y+1]]
             temp = filter(lambda square: -1 < square[0] < 8 and -1 < square[1] < 8, temp)
             legal_moves = list(filter(lambda square: self.board[square[0]][square[1]].color != self.turn, temp))
-        print(self.is_king_safe(start, end, self.turn))
-        return end in legal_moves and self.is_king_safe(start, end, self.turn)
+
+            # castling
+            if self.turn == 'white':
+                attacked = self.get_attacking('black')
+                if self.castling[0][0] and self.board[1][0].color is None and self.board[2][0].color is None and \
+                        self.board[3][0].color is None and [3, 0] not in attacked and [4, 0] not in attacked:
+                    legal_moves.append([2, 0])
+                if self.castling[0][1] and self.board[5][0].color is None and self.board[6][0].color is None and \
+                        [4, 0] not in attacked and [5, 0] not in attacked:
+                    legal_moves.append([6, 0])
+            else:
+                attacked = self.get_attacking('white')
+                if self.castling[1][0] and self.board[1][7].color is None and self.board[2][7].color is None and \
+                        self.board[3][7].color is None and [3, 7] not in attacked and [4, 7] not in attacked:
+                    legal_moves.append([2, 7])
+                if self.castling[1][1] and self.board[5][7].color is None and self.board[6][7].color is None and \
+                        [4, 7] not in attacked and [5, 7] not in attacked:
+                    legal_moves.append([6, 7])
+
+        return legal_moves
 
     # helper function that checks if a rook, bishop, queen move is legal
     def is_legal_helper(self, x, y, directions):
@@ -195,9 +239,47 @@ class Game:
                 temp_y += dir_y
         return legal_moves
 
+    def is_en_passant_safe(self, pawn_x):
+        attacked = []
+        if self.turn == 'white':
+            attacking = self.black_pieces
+        else:
+            attacking = self.white_pieces
+        for x, y in attacking:
+            piece = self.board[x][y]
+            if piece.piece == 'rook':
+                directions = [[1, 0], [-1, 0]]
+                attacked += self.is_en_passant_safe_helper(pawn_x, x, y, directions)
+            elif piece.piece == 'queen':
+                directions = [[1, 0], [-1, 0]]
+                attacked += self.is_en_passant_safe_helper(pawn_x, x, y, directions)
+
+        if self.turn == 'white':
+            return self.white_king not in attacked
+        else:
+            return self.black_king not in attacked
+
+    def is_en_passant_safe_helper(self, pawn_x, x, y, directions):
+        attacked = []
+        for dir_x, dir_y in directions:
+            temp_x = x + dir_x
+            temp_y = y + dir_y
+
+            while -1 < temp_x < 8 and -1 < temp_y < 8:
+                if [self.en_passant, 4] != [temp_x, temp_y] and [pawn_x, 4] != [temp_x, temp_y] and \
+                        self.turn == 'white' and self.board[temp_x][temp_y].color is not None:
+                    attacked.append([temp_x, temp_y])
+                    break
+                elif [self.en_passant, 3] != [temp_x, temp_y] and [pawn_x, 3] != [temp_x, temp_y] and \
+                        self.turn == 'black' and self.board[temp_x][temp_y].color is not None:
+                    attacked.append([temp_x, temp_y])
+                    break
+                temp_x += dir_x
+                temp_y += dir_y
+        return attacked
+
     # moves a piece
-    # TODO: implement special interactions for castling, promotion, and en passant
-    # TODO: check for check and checkmate
+    # TODO: check for checkmate
     def move(self, start, end):
         start_x = start[0]
         start_y = start[1]
@@ -205,22 +287,83 @@ class Game:
         end_y = end[1]
 
         piece = self.board[start_x][start_y]
+
+        if piece.piece == 'pawn':
+            if (piece.color == 'white' and end_y == 7) or (piece.color == 'black' and end_y == 0):
+                piece.piece = 'queen'
+
+            if (piece.color == 'white' and start_y == 1 and end_y == 3) or \
+                    (piece.color == 'black' and start_y == 6 and end_y == 4):
+                self.en_passant = start_x
+            else:
+                self.en_passant = None
+            if piece.color == 'white' and (start_x - end_x == 1 or start_x - end_x == -1) and \
+                    self.board[end_x][end_y].color is None:
+                self.board[end_x][end_y-1] = Piece()
+                self.black_pieces.remove([end_x, end_y-1])
+            if piece.color == 'black' and (start_x - end_x == 1 or start_x - end_x == -1) and \
+                    self.board[end_x][end_y].color is None:
+                self.board[end_x][end_y+1] = Piece()
+                self.white_pieces.remove([end_x, end_y+1])
+        else:
+            self.en_passant = None
+
         if piece.piece == 'king':
             if self.turn == 'white':
                 self.white_king = end
+                if start_x - end_x == 2:
+                    self.board[0][0] = Piece()
+                    self.board[3][0] = Piece('white', 'rook')
+                    self.castling[0] = [False, False]
+                    self.white_pieces.append([3, 0])
+                    self.white_pieces.remove([0, 0])
+                elif start_x - end_x == -2:
+                    self.board[7][0] = Piece()
+                    self.board[5][0] = Piece('white', 'rook')
+                    self.castling[0] = [False, False]
+                    self.white_pieces.append([5, 0])
+                    self.white_pieces.remove([7, 0])
             else:
                 self.black_king = end
+                if start_x - end_x == 2:
+                    self.board[0][7] = Piece()
+                    self.board[3][7] = Piece('black', 'rook')
+                    self.castling[1] = [False, False]
+                    self.black_pieces.append([3, 7])
+                    self.black_pieces.remove([0, 7])
+                elif start_x - end_x == -2:
+                    self.board[7][7] = Piece()
+                    self.board[5][7] = Piece('black', 'rook')
+                    self.castling[1] = [False, False]
+                    self.black_pieces.append([5, 7])
+                    self.black_pieces.remove([7, 7])
+
         self.board[start_x][start_y] = Piece()
         self.board[end_x][end_y] = piece
+
         if self.turn == 'white':
             self.white_pieces.remove(start)
             self.white_pieces.append(end)
             if end in self.black_pieces:
                 self.black_pieces.remove(end)
             self.turn = 'black'
+            if self.is_in_check('black'):
+                self.check_square = self.black_king
+            else:
+                self.check_square = []
         else:
             self.black_pieces.remove(start)
             self.black_pieces.append(end)
             if end in self.white_pieces:
                 self.white_pieces.remove(end)
             self.turn = 'white'
+            if self.is_in_check('white'):
+                self.check_square = self.white_king
+            else:
+                self.check_square = []
+
+    def is_in_check(self, color):
+        if color == 'white':
+            return self.white_king in self.get_attacking('black')
+        else:
+            return self.black_king in self.get_attacking('white')
--- main.py
+++ main.py
@@ -1,32 +1,56 @@
 import tkinter as tk
+from tkinter import font as tkfont
 from PIL import Image, ImageTk
 from game import Game
 
 
 class App:
     def __init__(self):
-        self.game = Game()
+        self.game = None
+        self.in_progress = False
         self.piece_images = [[None for _ in range(8)] for _ in range(8)]
         self.root = tk.Tk()
         self.root.title('Chess Online')
         self.root.geometry('800x500+150+150')
 
         self.board_image = None
+        self.threaten = None
         self.selection = None
         self.selection_square = []
+
+        button_font = tkfont.Font(size=12)
+        new_game_button = tk.Button(self.root, text='New Game', bd=1, width=9, height=2, command=self.new_game)
+        new_game_button['font'] = button_font
+        new_game_button.place(x=600, y=100)
+
+        status_font = tkfont.Font(size=18)
+        status_text = tk.Label(self.root, text='hi', font=status_font)
+        status_text.place(x=600, y=200)
 
         self.canvas = tk.Canvas(self.root, width=480, height=480)
         self.canvas.place(x=10, y=10)
         self.canvas.bind('<Button-1>', self.interact)
         self.render_board()
-        self.render_pieces()
+
         self.root.mainloop()
+
+    def new_game(self):
+        self.game = Game()
+        self.in_progress = True
+        self.render_pieces()
 
     def render_board(self):
         self.board_image = tk.PhotoImage(file='images/chessboard.png')
         self.canvas.create_image(0, 0, image=self.board_image, anchor='nw')
 
-    def render_selection(self):
+    def render_indicators(self):
+        if self.game.check_square:
+            self.threaten = tk.PhotoImage(file='images/threaten.png')
+            self.canvas.create_image(self.game.check_square[0] * 60 + 30,
+                                     (7 - self.game.check_square[1]) * 60 + 30, image=self.threaten)
+        else:
+            self.threaten = None
+
         if self.selection_square:
             self.selection = tk.PhotoImage(file='images/selection.png')
             self.canvas.create_image(self.selection_square[0] * 60 + 30,
@@ -51,6 +75,9 @@ class App:
             self.canvas.create_image(30 + 60 * x, 450 - 60 * y, image=self.piece_images[x][y])
 
     def interact(self, event):
+        if not self.in_progress:
+            return
+
         selection_square = [event.x // 60, 7 - (event.y // 60)]
 
         if (self.game.turn == 'white' and selection_square in self.game.white_pieces) or \
@@ -61,7 +88,7 @@ class App:
                 self.game.move(self.selection_square, selection_square)
             self.selection_square = []
 
-        self.render_selection()
+        self.render_indicators()
         self.render_pieces()
 
 
